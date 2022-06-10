90f04105c393c19aaef2a331f3dbd9766abb496c
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-10 18:42:04 +0300

    refactored Product:
    - changed type of creationDate to LocalDateTime.

--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -1,18 +1,18 @@
 package entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Product {
-    private long id;
+    private int id;
     private String name;
     private double price;
-    private String creationDate;
+    private LocalDateTime creationDate;
 }
 

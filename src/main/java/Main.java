1fb6ea8b8108431c9a4b95d6e1346b2d4133358a
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-10 18:39:42 +0300

    refactored Main:
    - deleted LoginServlet.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,19 @@
-import jakarta.servlet.DispatcherType;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import security.SecurityFilter;
-import security.SecurityService;
 import servlets.*;
-
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        SecurityService securityService = new SecurityService();
 
-        List<String> userTokens = new ArrayList<>();
-
-
-        context.addServlet(new ServletHolder((new AllProductsOfShopServlet())),"/shop");
-//        context.addServlet(new ServletHolder(new LaunchCssServlet()),"/css");
-        context.addServlet(new ServletHolder(new AllProductsServlet(userTokens)), "/products");
-        context.addServlet(new ServletHolder(new AddProductServlet(userTokens)), "/add");
-        context.addServlet(new ServletHolder(new DeleteProductServlet(userTokens)), "/delete");
-        context.addServlet(new ServletHolder(new UpdateProductServlet(userTokens)), "/update");
-        context.addServlet(new ServletHolder(new SearchProductServlet(userTokens)), "/products/search");
-        context.addServlet(new ServletHolder(new LoginServlet(userTokens)), "/login");
-        context.addFilter(new FilterHolder(new SecurityFilter(securityService)), "/add", EnumSet.of(DispatcherType.REQUEST));
+        context.addServlet(new ServletHolder((new AllProductsOfShopServlet())), "/");
+        context.addServlet(new ServletHolder(new AllProductsServlet()), "/products");
+        context.addServlet(new ServletHolder(new AddProductServlet()), "/products/add");
+        context.addServlet(new ServletHolder(new DeleteProductServlet()), "/products/delete");
+        context.addServlet(new ServletHolder(new UpdateProductServlet()), "/products/update");
+        context.addServlet(new ServletHolder(new SearchProductServlet()), "/products/search");
 
         Server server = new Server(8080);
         server.setHandler(context);

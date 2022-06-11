f1157349322c917139e74871781a94f15b95785a
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-11 12:23:42 +0300

    refactored Main:
    - add productService.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
+import dao.jdbc.JdbcProductDao;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import service.ProductService;
 import servlets.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        JdbcProductDao jdbcProductDao = new JdbcProductDao();
+        ProductService productService = new ProductService(jdbcProductDao);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
         context.addServlet(new ServletHolder(new AllProductsServlet()), "/products");
-        context.addServlet(new ServletHolder(new AddProductServlet()), "/products/add");
+        context.addServlet(new ServletHolder(new AddProductServlet(productService)), "/products/add");
         context.addServlet(new ServletHolder(new DeleteProductServlet()), "/products/delete");
         context.addServlet(new ServletHolder(new UpdateProductServlet()), "/products/update");
         context.addServlet(new ServletHolder(new SearchProductServlet()), "/products/search");

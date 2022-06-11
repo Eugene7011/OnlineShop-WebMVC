88e6907de3396ff47d9f4bb1a03b46eb38a271e9
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-11 12:23:10 +0300

    refactored AddProductServlet:
    - refactored doPost;
    - refactored doGet;
    - add getProductFromRequest.

--- src/main/java/servlets/AddProductServlet.java
+++ src/main/java/servlets/AddProductServlet.java
@@ -1,45 +1,49 @@
 package servlets;
 
-import dao.jdbc.JdbcProductDao;
 import entity.Product;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import pagegenerator.PageGenerator;
+import service.ProductService;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.util.Map;
 
 public class AddProductServlet extends HttpServlet {
+    private final ProductService productService;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        PageGenerator pageGenerator = PageGenerator.instance();
-        String page = pageGenerator.getPageWithEmptyMap("add_product.html");
-        try {
-            response.getWriter().write(page);
-        } catch (IOException exception) {
-            throw new RuntimeException("Can not get page with data from request");
-        }
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
+    public AddProductServlet(ProductService productService) {
+        this.productService = productService;
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        String name = request.getParameter("name");
-        int price = Integer.parseInt(request.getParameter("price"));
-        Product product = new Product();
-        product.setName(name);
-        product.setPrice(price);
-        product.setCreationDate(LocalDateTime.now());
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PageGenerator pageGenerator = PageGenerator.instance();
+        String page = pageGenerator.getPage("add_product.html");
+        response.getWriter().write(page);
+    }
 
-        response.setContentType("text/html;charset=utf-8");
-        JdbcProductDao jdbcProductDao = new JdbcProductDao();
-        jdbcProductDao.add(product);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
+            Product product = getProductFromRequest(request);
+            productService.add(product);
             response.sendRedirect("/products");
+
         } catch (IOException e) {
-            throw new RuntimeException("Data has not been added. Check and enter correct data in the fields", e);
+            String errorMessage = "Product has not been added. Check and enter correct data in the fields";
+            PageGenerator pageGenerator = PageGenerator.instance();
+
+            Map<String, Object> parameters = Map.of("errorMessage", errorMessage);
+            String page = pageGenerator.getPage("add_product.html", parameters);
+            response.getWriter().write(page);
         }
+    }
+
+    private Product getProductFromRequest(HttpServletRequest request) {
+        return Product.builder().
+                name(request.getParameter("name"))
+                .price(Double.parseDouble(request.getParameter("price")))
+                .build();
     }
 }
 

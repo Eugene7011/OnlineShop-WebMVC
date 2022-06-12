f5b09ffed7b83473a4a837a78620d6242667a3cd
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-12 23:55:21 +0300

    refactored Main:
    - add userTokens, securityService, LoginServlet.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,12 @@ import dao.jdbc.JdbcProductDao;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import security.SecurityService;
 import service.ProductService;
-import servlets.*;
+import web.servlets.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -12,11 +16,18 @@ public class Main {
         ProductService productService = new ProductService(jdbcProductDao);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
+        List<String> userTokens = new ArrayList<>();
+
+        SecurityService securityService = new SecurityService(userTokens);
+        AddProductServlet addProductServlet = new AddProductServlet(productService, securityService);
+
         context.addServlet(new ServletHolder(new AllProductsServlet()), "/products");
-        context.addServlet(new ServletHolder(new AddProductServlet(productService)), "/products/add");
+        context.addServlet(new ServletHolder(addProductServlet), "/products/add");
         context.addServlet(new ServletHolder(new DeleteProductServlet()), "/products/delete");
         context.addServlet(new ServletHolder(new UpdateProductServlet()), "/products/update");
         context.addServlet(new ServletHolder(new SearchProductServlet()), "/products/search");
+        context.addServlet(new ServletHolder(new LoginServlet(userTokens, securityService)), "/login");
+
 
         Server server = new Server(8080);
         server.setHandler(context);
@@ -24,3 +35,5 @@ public class Main {
         server.start();
     }
 }
+
+

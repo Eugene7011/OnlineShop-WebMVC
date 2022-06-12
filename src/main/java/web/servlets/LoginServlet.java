8f1acdc043a97db1419ebe959ebdec04c4212f3f
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-12 23:56:29 +0300

    add LoginServlet

---
+++ src/main/java/web/servlets/LoginServlet.java
@@ -0,0 +1,33 @@
+package web.servlets;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pagegenerator.PageGenerator;
+import security.SecurityService;
+
+import java.io.IOException;
+import java.util.List;
+
+public class LoginServlet extends HttpServlet {
+    private List<String> userTokens;
+    private SecurityService securityService;
+
+    public LoginServlet(List<String> userTokens, SecurityService securityService) {
+        this.userTokens = userTokens;
+        this.securityService = securityService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PageGenerator pageGenerator = PageGenerator.instance();
+        String page = pageGenerator.getPage("login.html");
+        response.getWriter().write(page);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.addCookie(securityService.generateCookie());
+        response.sendRedirect("/products");
+    }
+}

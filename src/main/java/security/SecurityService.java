9ba2cdbf4553c833835a05a05b024307301f676a
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-12 23:56:04 +0300

    add SecurityService

--- src/main/java/security/SecurityService.java
+++ src/main/java/security/SecurityService.java
@@ -1,20 +1,24 @@
 package security;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Stream;
+import java.util.UUID;
 
 public class SecurityService {
-    private final List<String> TOCKEN_STORAGE = Collections.synchronizedList(new ArrayList<>());
+    private List<String> userTokens;
 
-    public boolean isAuth(HttpServletRequest request, List<String> userTokens) {
-        boolean isAuth = false;
-        Cookie[] cookies = request.getCookies();
+    public SecurityService(List<String> userTokens) {
+        this.userTokens = userTokens;
+    }
+
+    public Cookie generateCookie() {
+        String userToken = UUID.randomUUID().toString();
+        userTokens.add(userToken);
+        return new Cookie("user-token", userToken);
+    }
+
+    public boolean isAuth(Cookie[] cookies) {
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("user-token")) {
@@ -26,12 +30,85 @@ public class SecurityService {
         }
         return false;
     }
-
-    public boolean isLoggedIn(Cookie[] cookies) {
-        if (Objects.isNull(cookies)) {
-            return false;
-        }
-        return Stream.of(cookies).anyMatch(cookie -> TOCKEN_STORAGE.contains(cookie.getValue()));
-    }
 }
+
+
+//    private final List<String> TOKEN_STORAGE = Collections.synchronizedList(new ArrayList<>());
+//
+//    public boolean isAuth(HttpServletRequest request, List<String> userTokens) {
+//        boolean isAuth = false;
+//        Cookie[] cookies = request.getCookies();
+//        if (cookies != null) {
+//            for (Cookie cookie : cookies) {
+//                if (cookie.getName().equals("user-token")) {
+//                    if (userTokens.contains(cookie.getValue())) {
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public boolean isLoggedIn(Cookie[] cookies) {
+//        if (Objects.isNull(cookies)) {
+//            return false;
+//        }
+//        return Stream.of(cookies).anyMatch(cookie -> TOKEN_STORAGE.contains(cookie.getValue()));
+//    }
+//
+
+
+//    public static final String PASSWORD_IS_NOT_VALID_MESSAGE = """
+//            Check password.It should be:""";
+//
+//    private static final Faker FAKER = new Faker();
+//    private static final String TOKEN_KEY = "user-token";
+//    private final List<String> TOKEN_STORAGE = Collections.synchronizedList(new ArrayList<>());
+//
+//    public boolean isLoggedIn(Cookie[] cookies) {
+//        if (Objects.isNull(cookies)) {
+//            return false;
+//        }
+//        return Stream.of(cookies)
+//                .anyMatch(cookie -> TOKEN_STORAGE.contains(cookie.getValue()));
+//    }
+//
+//    public Cookie generateCookie() {
+//        String tokenValue = getTokenValue();
+//        TOKEN_STORAGE.add(tokenValue);
+//        return new Cookie(TOKEN_KEY, tokenValue);
+//
+//    }
+//
+//    public void addSalt(User user) {
+//        String salt = generateSalt();
+//        String saltedPassword = user.getPassword() + salt;
+//        String hashedSaltPassword = hash(saltedPassword);
+//
+//        user.setPassword(hashedSaltPassword);
+//        user.setSalt(salt);
+//    }
+//
+//    public boolean isPasswordValid(String password, User existingUser) {
+//        String enteredPassword = hash(password + existingUser.getSalt());
+//        return enteredPassword.equals(existingUser.getPassword());
+//    }
+//
+//    public boolean isPasswordValid(String password) {
+//        return !(password == null || password.isBlank());
+//    }
+//
+//    String hash(String value) {
+//        return DigestUtils.md5Hex(value);
+//    }
+//
+//    String generateSalt() {
+//        return FAKER.random().hex(8);
+//    }
+//
+//    String getTokenValue() {
+//        return UUID.randomUUID().toString();
+//    }
+
 

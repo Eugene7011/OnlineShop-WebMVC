29503e1a9c7baeb002183e6f7dd1a845bc80786b
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-12 23:57:29 +0300

    refactored AddProductServlet:
    - add securityService.isAuth(request.getCookies())

--- src/main/java/web/servlets/AddProductServlet.java
+++ src/main/java/web/servlets/AddProductServlet.java
@@ -1,28 +1,39 @@
-package servlets;
+package web.servlets;
 
 import entity.Product;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import pagegenerator.PageGenerator;
+import security.SecurityService;
 import service.ProductService;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class AddProductServlet extends HttpServlet {
-    private final ProductService productService;
+    private ProductService productService;
+    private List<String> userTokens;
+    private SecurityService securityService;
 
-    public AddProductServlet(ProductService productService) {
+    public AddProductServlet(ProductService productService, SecurityService securityService) {
         this.productService = productService;
+        this.securityService = securityService;
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PageGenerator pageGenerator = PageGenerator.instance();
-        String page = pageGenerator.getPage("add_product.html");
-        response.getWriter().write(page);
+        if (securityService.isAuth(request.getCookies())) {
+            PageGenerator pageGenerator = PageGenerator.instance();
+            String page = pageGenerator.getPage("add_product.html");
+            response.getWriter().write(page);
+        } else {
+            response.sendRedirect("/login");
+        }
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
             Product product = getProductFromRequest(request);
@@ -39,7 +50,7 @@ public class AddProductServlet extends HttpServlet {
         }
     }
 
-    private Product getProductFromRequest(HttpServletRequest request) {
+    public Product getProductFromRequest(HttpServletRequest request) {
         return Product.builder().
                 name(request.getParameter("name"))
                 .price(Double.parseDouble(request.getParameter("price")))

39748d820a5e2791b6fc130416c32ab15ec1f222
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-15 00:20:26 +0300

    refactored Main:
    - add new FilterHolder(securityFilter) using path "/*".
    -

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import dao.jdbc.JdbcProductDao;
+import dao.jdbc.JdbcUserDao;
 import jakarta.servlet.DispatcherType;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.FilterHolder;
@@ -10,29 +11,33 @@ import web.security.SecurityFilter;
 import web.servlets.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        JdbcUserDao jdbcUserDao = new JdbcUserDao();
         JdbcProductDao jdbcProductDao = new JdbcProductDao();
         ProductService productService = new ProductService(jdbcProductDao);
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         List<String> userTokens = new ArrayList<>();
 
-        SecurityService securityService = new SecurityService(userTokens);
+        SecurityService securityService = new SecurityService(Collections.synchronizedList(userTokens), jdbcUserDao);
         AddProductServlet addProductServlet = new AddProductServlet(productService);
         SecurityFilter securityFilter = new SecurityFilter(securityService);
 
+        context.addServlet(new ServletHolder(new AllProductsServlet()), "/*");
         context.addServlet(new ServletHolder(new AllProductsServlet()), "/products");
         context.addServlet(new ServletHolder(addProductServlet), "/products/add");
         context.addServlet(new ServletHolder(new DeleteProductServlet()), "/products/delete");
         context.addServlet(new ServletHolder(new UpdateProductServlet()), "/products/update");
         context.addServlet(new ServletHolder(new SearchProductServlet()), "/products/search");
+
         context.addServlet(new ServletHolder(new LoginServlet(userTokens, securityService)), "/login");
-        context.addFilter(new FilterHolder(securityFilter), "/products", EnumSet.of(DispatcherType.REQUEST));
+        context.addFilter(new FilterHolder(securityFilter), "/*", EnumSet.of(DispatcherType.REQUEST));
 
         Server server = new Server(8080);
         server.setHandler(context);

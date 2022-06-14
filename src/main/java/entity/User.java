7436c5caa06b9c50bfd1bdccb497e8f2ab3bc90c
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-15 01:49:42 +0300

    add annotation:
    -@Getter, @Setter.

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -2,7 +2,11 @@ package entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Builder
 @AllArgsConstructor
 public class User {

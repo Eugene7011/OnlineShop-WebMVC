a7b72ef3d4101e70dd5b0cd14f0c9587c5a1a14e
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-14 15:18:00 +0300

    add UserRowMapper.

---
+++ src/main/java/dao/jdbc/mapper/UserRowMapper.java
@@ -0,0 +1,23 @@
+package dao.jdbc.mapper;
+
+import entity.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper {
+    public User mapRow(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String login = resultSet.getString("login");
+        String password = resultSet.getString("password");
+        String salt = resultSet.getString("salt");
+
+        User user = User.builder().
+                id(id)
+                .login(login)
+                .password(password)
+                .salt(salt)
+                .build();
+        return user;
+    }
+}

46f5c9dbb620ade8039ac36799f4abcd071ec8f0
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-16 02:35:47 +0300

    refactored UpdateProductServlet:
    - add private modifier to createPageVariablesMap;
    - add productService.

--- src/main/java/web/servlets/UpdateProductServlet.java
+++ src/main/java/web/servlets/UpdateProductServlet.java
@@ -1,11 +1,11 @@
 package web.servlets;
 
-import dao.jdbc.JdbcProductDao;
 import entity.Product;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import pagegenerator.PageGenerator;
+import service.ProductService;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -13,53 +13,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UpdateProductServlet extends HttpServlet {
+    private final ProductService productService;
+
+    public UpdateProductServlet(ProductService productService) {
+        this.productService = productService;
+    }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Map<String, Object> pageVariables = createPageVariablesMap(request);
         PageGenerator pageGenerator = PageGenerator.instance();
         String page = pageGenerator.getPage("updateproduct.html", pageVariables);
 
-        try {
-            response.getWriter().println(page);
-        } catch (IOException e) {
-            throw new RuntimeException("Cant get data from request");
-        }
+        response.getWriter().write(page);
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
         int id = Integer.parseInt(request.getParameter("id"));
         String name = request.getParameter("name");
         int price = Integer.parseInt(request.getParameter("price"));
         LocalDateTime creation_date = LocalDateTime.parse(request.getParameter("creation_date"));
 
-        response.setContentType("text/html;charset=utf-8");
         Product product = new Product(id, name, price, creation_date);
-
-        JdbcProductDao jdbcProductDao = new JdbcProductDao();
+        productService.update(product);
         try {
-            jdbcProductDao.update(product);
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("Cant edit product from database");
-        }
-        try {
+            response.setContentType("text/html;charset=utf-8");
             response.sendRedirect("/products");
-        } catch (IOException e) {
-            throw new RuntimeException("Cant show update products");
-        }
-        try {
-            response.getWriter().close();
         } catch (IOException exception) {
-            throw new RuntimeException("Cant show update products");
+            throw new RuntimeException("Can not update product");
         }
     }
 
-    static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+    private Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
         Map<String, Object> pageVariables = new HashMap<>();
         pageVariables.put("id", request.getParameter("id"));
 

89e0fd4f560ed66489407f02b0f69a8b132d4385
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-16 02:25:45 +0300

    refactored AllProductsServlet:
    - add constructor;
    - add productService.

--- src/main/java/web/servlets/AllProductsServlet.java
+++ src/main/java/web/servlets/AllProductsServlet.java
@@ -1,11 +1,11 @@
 package web.servlets;
 
-import dao.jdbc.JdbcProductDao;
 import entity.Product;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import pagegenerator.PageGenerator;
+import service.ProductService;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -13,14 +13,19 @@ import java.util.List;
 import java.util.Map;
 
 public class AllProductsServlet extends HttpServlet {
-    private JdbcProductDao jdbcProductDao = new JdbcProductDao();
+    private final ProductService productService;
+
+    public AllProductsServlet(ProductService productService) {
+        this.productService = productService;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         Map<String, Object> paramMap = new HashMap<>();
         List<Product> products;
+
         try {
-            products = jdbcProductDao.findAll();
+            products = productService.findAll();
             paramMap.put("products", products);
             PageGenerator pageGenerator = PageGenerator.instance();
             String page = pageGenerator.getPage("allproducts.html", paramMap);
@@ -29,7 +34,7 @@ public class AllProductsServlet extends HttpServlet {
             response.setStatus(HttpServletResponse.SC_OK);
             response.getWriter().write(page);
         } catch (IOException exception) {
-            throw new RuntimeException("Cant show all products from database");
+            throw new RuntimeException("Can not get all products from database");
         }
     }
 }

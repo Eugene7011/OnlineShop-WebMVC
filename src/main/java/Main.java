ba0a1356eebbd61e1dd81460984218a5c9172a06
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-16 00:55:26 +0300

    refactored Main:
    - deleted List<String> userTokens;
    - add securityFilter;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,36 +7,33 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import security.SecurityService;
 import service.ProductService;
+import service.UserService;
 import web.security.SecurityFilter;
 import web.servlets.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.EnumSet;
-import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
+
         JdbcUserDao jdbcUserDao = new JdbcUserDao();
         JdbcProductDao jdbcProductDao = new JdbcProductDao();
+
+        UserService userService = new UserService(jdbcUserDao);
         ProductService productService = new ProductService(jdbcProductDao);
 
+        SecurityService securityService = new SecurityService(userService);
+        SecurityFilter securityFilter = new SecurityFilter(userService);
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        List<String> userTokens = new ArrayList<>();
 
-        SecurityService securityService = new SecurityService(Collections.synchronizedList(userTokens), jdbcUserDao);
-        AddProductServlet addProductServlet = new AddProductServlet(productService);
-        SecurityFilter securityFilter = new SecurityFilter(securityService);
-
-        context.addServlet(new ServletHolder(new AllProductsServlet()), "/*");
-        context.addServlet(new ServletHolder(new AllProductsServlet()), "/products");
-        context.addServlet(new ServletHolder(addProductServlet), "/products/add");
-        context.addServlet(new ServletHolder(new DeleteProductServlet()), "/products/delete");
-        context.addServlet(new ServletHolder(new UpdateProductServlet()), "/products/update");
-        context.addServlet(new ServletHolder(new SearchProductServlet()), "/products/search");
-
-        context.addServlet(new ServletHolder(new LoginServlet(userTokens, securityService)), "/login");
+        context.addServlet(new ServletHolder(new AllProductsServlet(productService)), "/*");
+        context.addServlet(new ServletHolder(new AllProductsServlet(productService)), "/products");
+        context.addServlet(new ServletHolder(new AddProductServlet(productService)), "/products/add");
+        context.addServlet(new ServletHolder(new DeleteProductServlet(productService)), "/products/delete");
+        context.addServlet(new ServletHolder(new UpdateProductServlet(productService)), "/products/update");
+        context.addServlet(new ServletHolder(new SearchProductServlet(productService)), "/products/search");
+        context.addServlet(new ServletHolder(new LoginServlet(securityService)), "/login");
         context.addFilter(new FilterHolder(securityFilter), "/*", EnumSet.of(DispatcherType.REQUEST));
 
         Server server = new Server(8080);

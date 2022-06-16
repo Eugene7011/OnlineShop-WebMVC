9e6a7d2cce9ca2d17541e3a54aff06146dd940f9
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-16 15:59:38 +0300

    refactored LoginServlet:
    - changed import to web.util.PageGenerator;

--- src/main/java/web/servlets/LoginServlet.java
+++ src/main/java/web/servlets/LoginServlet.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import pagegenerator.PageGenerator;
+import web.util.PageGenerator;
 import security.SecurityService;
 
 import java.io.IOException;
@@ -19,7 +19,7 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PageGenerator pageGenerator = PageGenerator.instance();
+        PageGenerator pageGenerator = PageGenerator.getInstance();
         String page = pageGenerator.getPage("login.html");
         response.getWriter().write(page);
     }
@@ -35,7 +35,7 @@ public class LoginServlet extends HttpServlet {
             response.addCookie(new Cookie("user-token", token));
             response.sendRedirect("/*");
         } else {
-            PageGenerator pageGenerator = PageGenerator.instance();
+            PageGenerator pageGenerator = PageGenerator.getInstance();
             String page = pageGenerator.getPage("login.html");
             response.getWriter().write(page);
             response.getWriter().write("<h3 style=position:absolute;left:33%;>Please enter correct login and password! </h3>");

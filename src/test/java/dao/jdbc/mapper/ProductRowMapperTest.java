893a1c8550cd2e5322601ae382e97d39250b808c
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-16 20:14:38 +0300

    refactored ProductRowMapperTest:
    - changed import junit.

--- src/test/java/dao/jdbc/mapper/ProductRowMapperTest.java
+++ src/test/java/dao/jdbc/mapper/ProductRowMapperTest.java
@@ -9,8 +9,7 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 

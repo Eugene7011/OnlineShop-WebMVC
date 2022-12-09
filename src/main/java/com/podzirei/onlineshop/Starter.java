de9eac0c2810cad0b3f30abd10f1c3661ea44af0
Eugene7011 <e.podzirey@gmail.com>
2022-12-09 17:13:03 +0200

    - play with pom.xml

--- src/main/java/com/podzirei/onlineshop/Starter.java
+++ src/main/java/com/podzirei/onlineshop/Starter.java
@@ -2,8 +2,14 @@ package com.podzirei.onlineshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+//@ComponentScan(basePackages="com.podzirei.onlineshop.web.config")
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX,
+                pattern = "com.podzirei.onlineshop.*")})
 public class Starter {
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
